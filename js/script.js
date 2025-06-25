1774356b4ed9c8b3c2a37c230075e45b03d4bc98
artur-badalyan <badalyannaaa@gmail.com>
2025-06-25 19:09:55 +0400

    Added script and image changes

--- js/script.js
+++ js/script.js
@@ -1,45 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   (() => {
-    let now = new Date()
-    const fullDate = document.querySelectorAll('.fullDate')
-    let minDays
-    let dateFormat
-    const getFullDate = (d = null, mode) => {
-      now = new Date()
-      let day = now.getDate()
-      now.setDate(day - d)
-      if (mode == 'yy') {
-        return (now.getFullYear() +
-          '.' +
-          (now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1) +
-          '.' +
-          (now.getDate() < 10 ? '0' + now.getDate() : now.getDate()))
-      } else if (mode == 'mm') {
-        return ((now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1) +
-          '.' +
-          (now.getDate() < 10 ? '0' + now.getDate() : now.getDate()) +
-          '.' +
-          now.getFullYear())
-      } else if (mode == 'year') { return now.getFullYear() } else {
-        return ((now.getDate() < 10 ? '0' + now.getDate() : now.getDate()) +
-          '.' +
-          (now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1) +
-          '.' +
-          now.getFullYear())
-      }
-    }
-    if (fullDate) {
-      if (fullDate.length > 0) {
-        minDays = null
-        dateFormat = null
-        fullDate.forEach((elem) => {
-          if (elem.getAttribute('minDays')) {
-            minDays = +elem.getAttribute('minDays')
-            dateFormat = elem.getAttribute('dateMode')
-            elem.innerHTML = getFullDate(minDays, dateFormat)
-          } else { elem.innerHTML = getFullDate(0) }
-        })
-      }
-    }
+    const dateElements = document.querySelectorAll(".womanCom__date");
+
+    dateElements.forEach((el, index) => {
+      const now = new Date();
+      now.setDate(now.getDate() - index + 1);
+      const dateStr = `${now.getDate().toString().padStart(2, "0")}.${(now.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}.${now.getFullYear()}`;
+      el.textContent = dateStr;
+    });
   })();
 });
